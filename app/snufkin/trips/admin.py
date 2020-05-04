a7f1595fe07e8e51d847b483f8051e4ead363375
Karolina Kozicka <karolina.anna.kozicka@gmail.com>
2020-05-04 20:18:05 +0200

    Create admin panel

--- app/snufkin/trips/admin.py
+++ app/snufkin/trips/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Trip
 
-# Register your models here.
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ("name", "user", "start_date", "end_date")
+    search_fields = ("name",)
+    list_filter = ("user",)
